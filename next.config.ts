8aced42acc6e6b26cb3d991c7b9712b36c6e501a
Martin van Deursen <martin@twpf.online>
2026-07-27 12:36:04 +0200

    Clean up Next.js config for Bolt hosting

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  images: { unoptimized: true },
   reactStrictMode: true,
   webpack: (config, { isServer }) => {
     config.experiments = { ...config.experiments, asyncWebAssembly: true };
